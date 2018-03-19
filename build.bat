cd7b1b8e0633d9308d18372ac6df3584026121fe
Evans R <evansrutoh@gmail.com>
2018-03-19 20:52:56 +0800

    Primary batch file updates, reflect environment vars and paths

--- build.bat
+++ build.bat
@@ -1,17 +1,19 @@
 rem @echo off
-set CYGWINDIR=d:\cygwin
-set THISDIR=l:\Projects\ClamWin\0.90\clamwin
-set ISTOOLDIR=C:\Program Files (x86)\ISTool
+set CYGWINDIR=C:\cygwin64
+set THISDIR=F:\Dev\code\clamwin-code
+set ISTOOLDIR=C:\Program Files (x86)\Inno Setup 5
 set SEVENZIP=C:\Program Files\7-Zip\7z.exe
-set UPX_UTIL=C:\tools\upx.exe
+set UPX_UTIL=E:\IDMDownloads\upx394w\upx.exe
 
 set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 8\VC
 
 set MSSDKDIR=C:\Program Files\Microsoft Platform SDK
-set PYTHONDIR=C:\python23
+set PYTHONDIR=C:\Python23
 
-set WGET_UTIL=c:\tools\wget.exe
+set WGET_UTIL=E:\ProgramFilesMpya\GnuWin32\bin\wget.exe
 set DB_MIRROR=db.au.clamav.net
+
+set BASEVARSSET=1
 
 rem build pyclamav
 cd ..\addons\pyc
@@ -22,20 +24,20 @@ rmdir build\temp.win32-2.3\Release
 rmdir build\temp.win32-2.3
 rmdir build
 
-call build.cmd release
+call build.bat release
 if not "%ERRORLEVEL%"=="0" goto ERROR
 copy .\build\lib.win32-2.3\pyc.pyd "%THISDIR%\py"
 if not "%ERRORLEVEL%"=="0" goto ERROR
 
 rem build ExplorerShell
-rem cd %THISDIR%\cpp
-rem call build.bat
-rem if not "%ERRORLEVEL%"=="0" goto ERROR
-rem cd ..\
+cd %THISDIR%\cpp
+call build.bat release
+if not "%ERRORLEVEL%"=="0" goto ERROR
+cd ..\
 rem build BalloonTip.pyd
-rem cd py\BalloonTip
-rem call build.bat
-rem if not "%ERRORLEVEL%"=="0" goto ERROR
+cd py\BalloonTip
+call build.bat
+if not "%ERRORLEVEL%"=="0" goto ERROR
 
 rem build py2exe binaries
 cd %THISDIR%\setup\py2exe
@@ -50,7 +52,7 @@ rem del clamwin.zip
 rem cd library
 rem call "%SEVENZIP%" a -tzip -mx9 ..\clamwin.zip -r
 rem cd ..
-rd library /s /q 
+rem rd library /s /q 
 
 rem upx all files now
 rem call "%UPX_UTIL%" -9 *.* 
@@ -61,23 +63,23 @@ rem cd ..\..\..\..\
 
 
 rem get the latest db files
-call %WGET_UTIL% http://%DB_MIRROR%/main.cvd -N -O "%THISDIR%\Setup\cvd\main.cvd"
-if not "%ERRORLEVEL%"=="0" goto ERROR
-call %WGET_UTIL% http://%DB_MIRROR%/daily.cvd -N -O "%THISDIR%\Setup\cvd\daily.cvd"
-if not "%ERRORLEVEL%"=="0" goto ERROR
-call %WGET_UTIL% http://%DB_MIRROR%/bytecode.cvd -N -O "%THISDIR%\Setup\cvd\bytecode.cvd"
-if not "%ERRORLEVEL%"=="0" goto ERROR
+::call %WGET_UTIL% http://%DB_MIRROR%/main.cvd -N -O "%THISDIR%\Setup\cvd\main.cvd"
+::if not "%ERRORLEVEL%"=="0" goto ERROR
+::call %WGET_UTIL% http://%DB_MIRROR%/daily.cvd -N -O "%THISDIR%\Setup\cvd\daily.cvd"
+::if not "%ERRORLEVEL%"=="0" goto ERROR
+::call %WGET_UTIL% http://%DB_MIRROR%/bytecode.cvd -N -O "%THISDIR%\Setup\cvd\bytecode.cvd"
+::if not "%ERRORLEVEL%"=="0" goto ERROR
 
 
-call "%ISTOOLDIR%\ISTool.exe" -compile "%THISDIR%\Setup\Setup-nodb.iss"
+call "%ISTOOLDIR%\Compil32.exe" /cc "%THISDIR%\Setup\Setup-nodb.iss"
 if not "%ERRORLEVEL%"=="0" goto ERROR
 rem move nodb setup to -nodb file
 del "%THISDIR%\Setup\Output\Setup-nodb.exe"
-move "%THISDIR%\Setup\Output\Setup.exe" "%THISDIR%\Setup\Output\Setup-nodb.exe"
+move "%THISDIR%\Setup\Output\mysetup.exe" "%THISDIR%\Setup\Output\Setup-nodb.exe"
 if not "%ERRORLEVEL%"=="0" goto ERROR
 
 
-call "%ISTOOLDIR%\ISTool.exe" -compile "%THISDIR%\Setup\Setup.iss"
+call "%ISTOOLDIR%\Compil32.exe" /cc "%THISDIR%\Setup\Setup.iss"
 if not "%ERRORLEVEL%"=="0" goto ERROR
 
 goto END
